36eb128e47b83e29158648bbdfbe65f496cd6c6d
diegolimal90 <diego.lima.lopes@hotmail.com.br>
2017-09-03 22:54:10 -0300

    estruturando node.js para o bot

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var express = require('express');
 var request = require('request');
 var bodyParser = require('body-parser');
@@ -13,4 +15,9 @@ app.get("/", function(request, response){
 	
 	res.send("tudo funcionando uai!");
 	console.log("tudo funcionando eita que eu to ficando bom hahahaha");
+});
+
+
+app.listen(process.env.PORT || 3000, function(){
+	console.log("Servidor Express funcionando na porta %d no modo %s", this.address().port, app.settings.env);
 });
