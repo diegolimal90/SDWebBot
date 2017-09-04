303d688e007c868534d686da46462ef7302d63de
diegolimal90 <diego.lima.lopes@hotmail.com.br>
2017-09-03 22:44:29 -0300

    estruturando node.js para o bot

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// JavaScript Document
+var express = require('express');
+var request = require('request');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+
+app.get("/", function(req, res){
+	
+	res.send("tudo funcionando uai!");
+	console.log("tudo funcionando eita que eu to ficando bom hahahaha");
+});
