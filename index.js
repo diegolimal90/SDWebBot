56653acc70dd7de694a6efc2ef035fcea6a5f528
diegolimal90 <diego.lima.lopes@hotmail.com.br>
2017-09-04 02:17:04 -0300

    validação do webhook

--- index.js
+++ index.js
@@ -17,6 +17,18 @@ app.get("/", function(request, response){
 	console.log("tudo funcionando eita que eu to ficando bom hahahaha");
 });
 
+app.get("/webhook", function(req, res){
+	if(req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === 'sdwebbot1290'){
+		console.log("Webhook validado com sucesso!");
+		res.status(200).send(req.query['hub.challenge']);		
+	}else{
+		console.log("Falha na validação do webhook.");
+		res.sendStatus(403);
+	}
+});
+
+
+
 
 app.listen(process.env.PORT || 3000, function(){
 	console.log("Servidor Express funcionando na porta %d no modo %s", this.address().port, app.settings.env);
